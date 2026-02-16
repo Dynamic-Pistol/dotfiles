973a5bc50f230f9b51fc36912da457cb7b08d811
Malek Essam Zakaria Darwish <spidermalek666@gmail.com>
2026-02-16 22:10:31 +0200

    Fixed mango issues

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -2,9 +2,13 @@ regreet & >/dev/null 2>&1 &
 
 wbg /home/kyle/Pictures/background.jpg & >/dev/null 2>&1 &
 
-waybar & >/dev/null 2>&1 &
+swaync -c ~/.config/mango/swaync/config.jsonc -s ~/.config/mango/swaync/style.css >/dev/null 2>&1 &
 
-mako & >/dev/null 2>&1 &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1 &
+
+waybar >/dev/null 2>&1 &
+
+vicinae server &
 
 # keep clipboard content
 wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
@@ -12,7 +16,6 @@ wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 # clipboard content manager
 wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1 &
 
 /usr/lib/soteria-polkit/soteria & >/dev/null 2>&1 &
 
